81a19b6345f53db0469b33bf56754bdce58b8c96
chandnana <achanachandana@gmil.com>
2024-12-08 12:49:39 +0530

    initial commit

---
+++ index.java
@@ -0,0 +1,22 @@
+public class index {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers from 1 to 100 are:");
+        
+        for (int num = 2; num <= 100; num++) {
+            boolean isPrime = true;
+            
+            // Check if the number is prime
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            
+            // If number is prime, print it
+            if (isPrime) {
+                System.out.println(num);
+            }
+        }
+    }
+}
